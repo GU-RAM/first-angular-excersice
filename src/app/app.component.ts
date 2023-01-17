172382f7fc9938dff37916648cbcf8512187cf4d
gurami <guramidgebuadze@gmail.com>
2023-01-17 23:49:19 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
 
+interface Todo {
+  todo: string;
+  difficulty: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'todo-list';
+
+  show = true;
+  taskName: string = '';
+  difficultyChosen: string = '';
+
+  todos: Todo[] = [];
+
+  difficulty: string[] = ['Easy', 'Medium', 'Hard'];
+  addTodo() {
+    if (this.taskName && this.difficultyChosen) {
+      this.todos.push({
+        todo: this.taskName,
+        difficulty: this.difficultyChosen,
+      });
+      this.taskName = '';
+      this.difficultyChosen = '';
+    }
+  }
+  inProgress: Todo[] = [];
+  done: Todo[] = [];
 }
